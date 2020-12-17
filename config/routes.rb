7b8fe430f5e2b01700db47ce7d86f6e67c3b8b7d
Sean <Sransonette@gmail.com>
2020-12-16 23:50:29 -0600

    add logout button to trainer show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-
   resources :pokemon
   get '/index', to: 'pokemon#index'
   resources :trainers, only: [:new, :create, :show] do
